21f78f5b9c191bf6f3e940eab76dc7d124544bd6
Shivam Sikdar <sikdarshivam@gmail.com>
2021-09-06 12:40:06 +0530

    Objects and class in it

---
+++ obj.js
@@ -0,0 +1,14 @@
+class User{
+  constructor(name, learner, address) {
+    this.name = name;
+    this.learner = learner;
+    this.address = address;
+    
+  }
+  
+}
+var learnOne = new User("Draco", "Shivam", "Kolkata");
+var learnTwo = new User("Leo", "Arun", "Mumbai" );
+
+console.log(learnOne);
+console.log(learnTow);

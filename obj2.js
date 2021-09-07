3c8b29872f97bda21176394559fa061f657aea5e
Shivam Sikdar <sikdarshivam@gmail.com>
2021-09-07 21:36:48 +0530

    Object without using class

---
+++ obj2.js
@@ -0,0 +1,12 @@
+function User(name, learner, address) {
+    this.name = name;
+    this.learner = learner;
+    this.address = address;
+    
+  }
+  
+var learnOne = new User("Draco", "Shivam", "Kolkata");
+var learnTwo = new User("Leo", "Arun", "Mumbai" );
+
+console.log(learnOne);
+console.log(learnTwo);

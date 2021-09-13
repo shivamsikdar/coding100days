f968a6ce15fbaadf445389982dc3eddb54f7374e
Shivam Sikdar <sikdarshivam@gmail.com>
2021-09-13 14:02:10 +0530

    Post img in homepage using react js(still working)

---
+++ day8.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <br/><br/>
+<img src= "http://lorempixel.com/g/400/200/" heigt={200} width={300} /><br/><br/>
+<img src= "http://lorempixel.com/400/200/sports/Dummy-Text/" heigt={200} width={300} /><br/><br/>
+<img src= "http://lorempixel.com/400/200/" heigt={200} width={300} /><br/><br/>
+<img src= "http://lorempixel.com/400/200/sports/1/" heigt={200} width={300} /><br/>
+    </div>
+  );
+}
+
+export default App;

9a1f109a72ab549036d0d08be35202c4b6dd9962
Shivam Sikdar <sikdarshivam@gmail.com>
2021-09-28 23:25:44 +0530

    program to pass parameter to a setTimeout function

---
+++ day23.js
@@ -0,0 +1,9 @@
+// program to pass parameter to a setTimeout() function
+
+function greet() {
+    console.log('Hello world');
+}
+
+// passing parameter
+setTimeout(greet, 3000);
+console.log('This message is shown first');

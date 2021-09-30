ba2f238c2c58820a40e2798ad87b819824c0b717
Shivam Sikdar <sikdarshivam@gmail.com>
2021-09-30 23:07:44 +0530

    program to perform function overloading

---
+++ day25.js
@@ -0,0 +1,25 @@
+// program to perform function overloading
+function sum() {
+    switch (arguments.length) {
+    case 0:
+        console.log('You have not passed any argument');
+        break;
+    case 1:
+        console.log('Pass at least two arguments');
+        break;
+    default:
+        let result = 0;
+        let length = arguments.length;
+    
+        for (i = 0; i < length; i++) {  
+            result = result + arguments[i];  
+        }  
+        console.log(result);
+        break;
+    }
+}
+
+sum();
+sum(5); 
+sum(5, 9);    
+sum(1, 2, 3, 4, 5, 6, 7, 8, 9);

102c44576e0235d4c1b8df8e63f0c5497129ef64
Shivam Sikdar <sikdarshivam@gmail.com>
2021-10-26 23:35:50 +0530

    program to replace all line breaks in a string with <br>

---
+++ day36.js
@@ -0,0 +1,8 @@
+// program to replace all line breaks in a string with <br>
+const string = `I am Learning JavaScript.
+JavaScript is fun.
+JavaScript is easy.`;
+
+const result = string.replace(/(\r\n|\r|\n)/g, '<br>');
+
+console.log(result);

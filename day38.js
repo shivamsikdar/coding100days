37dbed9acba417d158e43d85171a177392d2b6a1
Shivam Sikdar <sikdarshivam@gmail.com>
2021-10-28 23:13:49 +0530

    program to get the file extension

---
+++ day38.js
@@ -0,0 +1,15 @@
+// program to get the file extension
+
+function getFileExtension(filename){
+
+    // get file extension
+    const extension = filename.split('.').pop();
+    return extension;
+}
+
+// passing the filename
+const result1 = getFileExtension('module.js');
+console.log(result1);
+
+const result2 = getFileExtension('module.txt');
+console.log(result2);

69edcff9aaf75b3fd79dc3a8e10295fce09fcb1c
Shivam Sikdar <sikdarshivam@gmail.com>
2021-10-30 14:06:36 +0530

    program to set default parameter value

---
+++ day40.js
@@ -0,0 +1,11 @@
+// program to set default parameter value
+
+function sum(x = 3, y = 5) {
+
+    // return sum
+    return x + y;
+}
+
+console.log(sum(5, 15));
+console.log(sum(7));
+console.log(sum());

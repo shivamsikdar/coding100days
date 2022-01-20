0abcc6dc9c47e4116975445ae35d61edceaeaa95
Shivam Sikdar <sikdarshivam@gmail.com>
2022-01-20 22:59:19 +0530

    Find minimum number of coins that make a given value(recursion)

---
+++ day74.js
@@ -0,0 +1,35 @@
+//Find minimum number of coins that make a given value(recursion)
+
+function minCoins(coins, m, V) {
+      
+    if (V == 0)
+        return 0;
+    
+    
+    let res = Number.MAX_VALUE;
+    
+    for(let i = 0; i < m; i++)
+    {
+        if (coins[i] <= V)
+        {
+            let sub_res = minCoins(coins, m,
+                               V - coins[i]);
+            
+            
+            if (sub_res != Number.MAX_VALUE && 
+                sub_res + 1 < res)
+                res = sub_res + 1;
+        }
+    }
+    return res;
+}
+
+
+let coins = [ 9, 6, 5, 1 ];
+let m = coins.length;
+let V = 11;
+
+console.log("Minimum coins required is " +
+               minCoins(coins, m, V) );
+
+

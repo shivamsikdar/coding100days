f9e93ea74f1816d36e075973f9e2b28d5ece5a6f
Shivam Sikdar <sikdarshivam@gmail.com>
2022-01-29 22:57:53 +0530

    Array challenge

---
+++ day81.js
@@ -0,0 +1,9 @@
+//Array challenge
+
+  let digits = [1,2,3];
+  var plusOne = function(digits) {
+    let digitsJoined=digits.join('');
+    let digitsNumber=Number(digitsJoined)+1;
+    return digitsNumber.toString().split('').map(x=>Number(x));
+};
+console.log(plusOne(digits));

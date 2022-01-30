6eb041b226afbc46ea9c3c50f117b1ad7ae28ee4
Shivam Sikdar <sikdarshivam@gmail.com>
2022-01-30 21:06:51 +0530

    Oddball Sum

---
+++ day82.js
@@ -0,0 +1,23 @@
+// Oddball Sum
+
+function oddballSum(nums) {
+ 
+    // final count of all odd numbers added up
+    var final_count = 0;
+    
+    // loop through entire list
+    for (var i = 0; i < nums.length; i++) {
+      
+      // we divide by 2, and if there is a remainder then
+      // the number must be odd so we add it to final_count
+      if (nums[i] % 2 === 1) {
+        final_count += nums[i]
+      }
+      
+    }
+    
+    return final_count;
+    
+  }
+  
+  console.log(oddballSum([1, 2, 3, 4, 5])); 

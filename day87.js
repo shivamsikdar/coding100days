64db45c6bbd6f4dc9822e377d35860241d6251c4
Shivam Sikdar <sikdarshivam@gmail.com>
2022-02-08 23:06:16 +0530

    asynchronous programming using asnyc / await

---
+++ day87.js
@@ -0,0 +1,26 @@
+//asynchronous programming using asnyc / await
+
+const getTodo = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let error = true;
+            if(!error)
+                resolve({ text: 'Complete Code Example' })
+            else
+                reject()
+        }, 2000)     
+    })
+}
+
+async function fetchTodo () {
+    try { 
+        const todo = await getTodo()
+        return todo
+    } catch (error) {
+        console.log("Error occurred")
+    }
+}
+
+fetchTodo()
+    .then(todo => console.log(todo.text))
+    .catch(() => console.log("Error occurred"))

239a396b5c87cbcd141368c5600c576affc9b914
Shivam Sikdar <sikdarshivam@gmail.com>
2022-02-11 21:31:30 +0530

    Asynchronus Using Promise

---
+++ day88.js
@@ -0,0 +1,17 @@
+//Asynchronus Using Promise
+
+const getTodo = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let error = false;
+            if(!error)
+                resolve({ text: 'Complete Code Example' })
+            else
+                reject()
+        }, 2000)     
+    })
+}
+
+getTodo().then(todo => {
+    console.log(todo.text)
+})

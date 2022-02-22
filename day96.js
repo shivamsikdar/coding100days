b289150ac72078c1dd773d45317bdd6324c26991
Shivam Sikdar <sikdarshivam@gmail.com>
2022-02-22 22:48:41 +0530

    Basic practice of map and filter function in JS

---
+++ day96.js
@@ -0,0 +1,16 @@
+//map and filter practice
+
+// //map()
+var arr=[1,2,3,4,5,6]
+
+var myResult= arr.map(x=>x+3);
+
+console.log(myResult);
+
+//filter
+
+var arr=[1,2,3,4,5,6]
+
+var myFill= arr.filter(x=>x>3);
+
+console.log(myFill);

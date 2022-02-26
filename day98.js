04d13ba1c2d1c776c643ab28c3c84e934558e5be
Shivam Sikdar <sikdarshivam@gmail.com>
2022-02-26 22:34:53 +0530

    Use a Mixin Unrelated Objects

---
+++ day98.js
@@ -0,0 +1,20 @@
+//Use a Mixin to Add Common Behavior Between Unrelated Objects
+
+let bird = {
+  name: "Donald",
+  numLegs: 2
+};
+
+let boat = {
+  name: "Warrior",
+  type: "race-boat"
+};
+
+
+let glideMixin = function(obj) {
+  obj.glide = function() {
+    console.log("Gliding!");
+  };
+};
+glideMixin(bird);
+glideMixin(boat);

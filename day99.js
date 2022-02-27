93c783f7487fe93fd152c846afeeee8d1734dbcd
Shivam Sikdar <sikdarshivam@gmail.com>
2022-02-27 23:15:03 +0530

    Override Inherited Methodss

---
+++ day99.js
@@ -0,0 +1,20 @@
+//Override Inherited Methodss
+
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+
+
+Penguin.prototype.fly = function() {
+  return "Alas, this is a flightless bird.";
+};
+
+
+
+let penguin = new Penguin();
+console.log(penguin.fly());
